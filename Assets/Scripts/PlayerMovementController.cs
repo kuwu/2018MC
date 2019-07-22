2f55a962cd0ed411ddb9030767aeb7292a63efcf
truk_w@outlook.com <truk_w@outlook.com>
2019-07-22 14:48:58 +0900

    Intial project add

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterGrounding))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovementController : MonoBehaviour
+{ 
+    [SerializeField]
+    private float moveSpeed = 2;
+    [SerializeField]
+    private float jumpForce = 400;
+
+    private new Rigidbody2D rigidbody2D;
+    private CharacterGrounding characterGrounding;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        characterGrounding = GetComponent<CharacterGrounding>();
+    }
+    
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+      
+
+        Vector3 movement = new Vector3(horizontal, 0);
+
+        transform.position += movement * Time.deltaTime * moveSpeed;
+
+        if (Input.GetButtonDown("Fire1") && characterGrounding.IsGrounded)
+        {
+            
+            rigidbody2D.AddForce(Vector2.up * jumpForce);
+                  
+        }
+
+    }
+}
